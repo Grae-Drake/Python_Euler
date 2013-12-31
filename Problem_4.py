7945789eb40d8dc3828adf69ad5f4816a686bb18
Grae Drake <graemdrake@gmail.com>
2013-12-31 00:17:42 -0500

    Refactor Problem 4

--- Problem_4.py
+++ Problem_4.py
@@ -1,15 +1,24 @@
-pallindrome = []
-range999 = []
-for i in range(1,1000):
-        range999.append(i)
-for i in range999:
-        for x in range(1,1000):
-                temp = x*i
-                check1 = str(temp)
-                check2 = check1[::-1]
-                if check1 == check2:
-                        pallindrome.append(temp)
-                
-pallindrome.sort()
-print(pallindrome)
+# Problem 4: Largest Palindrome Product
+def isPallindrome(n):
+
+	# Returns True if n is a palindrome, otherwise returns False.
+	if str(n) == str(n)[::-1]:
+		return True
+	else:
+		return False
+
+def main(limit):
+
+	# Returns the largest palindrome that is the product of two
+	# limit-digit numbers.
+	pallindrome = []
+	for i in range(10 ** (limit-1), 10 ** limit):
+		for j in range(10 ** (limit-1), 10 ** limit):
+			candidate = i * j
+			if isPallindrome(candidate):
+				pallindrome.append(candidate)
+	pallindrome.sort()
+	return pallindrome[-1]
+
+print(main(3))
 
