8f2ac6e77cba7a15e9637e5d94c759a90a7d243f
Grae Drake <graemdrake@gmail.com>
2013-12-31 02:57:36 -0500

    Refactor Problem 7

--- Problem_7.py
+++ Problem_7.py
@@ -1,13 +1,17 @@
-primes = [2]
-counter = 3
+# Problem 7: 10001st prime
+import sys
+sys.path.append("/Users/graedrake/Documents/Projects/Python_Euler/Functions")
 
-while len(primes) <= 10000:
-        if any(counter%x == 0 for x in primes):
-                counter +=1
-        else:
-                primes.append(counter)
-                counter +=1
-                
-        
+import Euler
 
-print(primes[-1])
+def main(n):
+
+	# Returns the nth prime number.
+	counter = 0
+	testnum = 1
+	while counter < n:
+		testnum = Euler.nextPrime(testnum)
+		counter += 1
+	return testnum
+
+print(main(10001))
