abc54377cfc6eded9c227669f92cb5a017b66d74
Grae Drake <graemdrake@gmail.com>
2013-12-31 01:29:48 -0500

    Refactor Problem 5

--- Problem_5.py
+++ Problem_5.py
@@ -1,24 +1,33 @@
-range20 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-primes = [2,3,5,7,11,13,17,19]
+# Problem 5: Smallest multiple
 
-primeFactors = []
+import math
 
-counter = 1
+def isPrime(n):
 
-for x in primes:
-        while any(i%x == 0 for i in range20):
-                for i in range20:
-                        if i % x == 0:
-                                primeFactors.append(x)
-                                range20.remove(i)
-                                range20.append(i/x)
+	# Returns True if n in prime; otherwise returns False
+	if n == 2 or n == 3: return True
+	if n < 2 or n%2 == 0: return False
+	if n < 9: return True
+	if n%3 == 0: return False
+	r = int(n ** 0.5)
+	f = 5
+	while f <= r:
+		if n % f == 0: return False
+		if n % (f + 2) == 0: return False
+		f += 6
+	return True
 
-for i in primeFactors:
-        counter *= i
+def main(limit):
 
-print(primeFactors)
+	# Iterates through each number less than limit testing for primeness.
+	# Counter is multiplied by each prime number for the a number of times
+	# equal to the greatest number of times that prime divides a number less
+	# than or equal to the limit.  For example, 2 divides 8 3 times (8 = 2*2*2),
+	# so when limit == 10 counter is multiplied by 2^3.
+	counter = 1
+	for x in range(2, limit+1):
+		if isPrime(x):
+			counter *= (x ** math.floor(math.log(limit, x)))
+	return counter
 
-print(range20)
-
-print(counter)
-
+print(main(20))
