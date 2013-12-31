67991702c68012d3deb011db2ce024a0f6fa1b1c
Grae Drake <graemdrake@gmail.com>
2013-12-31 02:30:40 -0500

    Tweak Prob 5 module path

--- Problem_5.py
+++ Problem_5.py
@@ -1,21 +1,8 @@
 # Problem 5: Smallest multiple
+import sys
+sys.path.append("/Users/graedrake/Documents/Projects/Python_Euler/Functions")
 
-import math
-
-def isPrime(n):
-
-	# Returns True if n in prime; otherwise returns False
-	if n == 2 or n == 3: return True
-	if n < 2 or n%2 == 0: return False
-	if n < 9: return True
-	if n%3 == 0: return False
-	r = int(n ** 0.5)
-	f = 5
-	while f <= r:
-		if n % f == 0: return False
-		if n % (f + 2) == 0: return False
-		f += 6
-	return True
+import Euler, math
 
 def main(limit):
 
@@ -26,7 +13,7 @@ def main(limit):
 	# so when limit == 10 counter is multiplied by 2^3.
 	counter = 1
 	for x in range(2, limit+1):
-		if isPrime(x):
+		if Euler.isPrime(x):
 			counter *= (x ** math.floor(math.log(limit, x)))
 	return counter
 
