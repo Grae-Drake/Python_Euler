f35ffe767ca705f5d9f0a9fa038c4254a1333d55
Grae Drake <graemdrake@gmail.com>
2013-12-31 21:04:39 -0500

    Refactor Problem 10 (Woo)

--- Problem_10.py
+++ Problem_10.py
@@ -1,23 +1,26 @@
-def prime_sieve(limit):
-    
-        # create an empty start list and a counter
-    start_list = []
-    counter = 0
-    root_limit = int(limit**.5 + 1)
-    
-        # populate the list with (limit-2) instances of "True"
-        # remember that the list is not inclusive and we're ommitting 1
-    for i in range(1,limit): 
-        start_list.append(True)
+# Problem 10: Summation of primes
 
-        # flip the bits!
-    for index1, value1 in enumerate(start_list):
-        if (value1 == True) and (index1 > 1):
-            for index2, value2 in enumerate(start_list):
-                if index2 => index1**2:
-                    value2 = False
-                    
-        else:
-            pass
+import math
 
-prime_sieve(10)
+def primeSieve(limit):
+    
+    # Returns a list of prime numbers below limit.
+    array = [True for x in (range(limit))]      # Initialize array of booleans.
+    root = math.ceil(limit ** .5)
+    counter = 2                                 # Start sieving with number 2.
+    result = []
+    while counter <= root:
+        if array[counter] == True:
+            # Step through array and replace multiples of each prime number with False.
+            array[counter * 2::counter] = [False for x in range(len(array[counter * 2::counter]))]
+        counter += 1
+    for x, y in enumerate(array):
+        if y and x > 1:
+            result.append(x)
+    return result
+
+def main(limit):
+
+    # Returns the sum of all prime numbers below limit.
+    return sum(primeSieve(limit))
+print main(2000000)
