8875b39010973956b420eb0ba64b2616eec8196c
Grae Drake <graemdrake@gmail.com>
2014-01-04 23:48:25 -0500

    Refactor Problem 14

--- Problem_14.py
+++ Problem_14.py
@@ -1,26 +1,34 @@
-def big_c_seq(limit):
-    count = 1
-    chain_lengths = []
-    while count <= limit-1:
-        chain_lengths.append([count, c_fun(count)])
-        count += 1
-    chain_lengths.sort(key=lambda x: x[1])
-    print(chain_lengths[-1])
-    
+# Problem 14: Longest Collatz sequence  
 
-def c_fun(x):
+import time
+t1 = time.clock()
+
+def getSequenceLength(n):
+
+    # Returns the number of elements in n's Collatz sequence
     counter = 1
-    var = x
+    var = n
     while var != 1:
         if var % 2 == 0:
             var = var/2
             counter += 1
         else:
-            var = 3*var + 1
+            var = 3 * var + 1
             counter += 1
-           
     return counter
 
+def main(limit):
 
+    # Returns the number less than limit with the longest Collatz sequence
+    result = 0
+    longestSequence = 0
+    for n in range(1,limit):
+        sequenceLength = getSequenceLength(n)
+        if sequenceLength > longestSequence:
+            longestSequence = sequenceLength
+            result = n
+    return result
     
-big_c_seq(1000000)
+print main(1000000)
+
+print "Execution time: " + str(time.clock() - t1) + " seconds"
