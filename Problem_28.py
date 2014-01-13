4e0526dc8703bcf0cd106eef08a011b9c0cfcff7
Grae Drake <graemdrake@gmail.com>
2014-01-12 22:30:39 -0500

    Refactor Problem 28

--- Problem_28.py
+++ Problem_28.py
@@ -1,13 +1,19 @@
-        # Problem 28: Number Spiral Diagonals
+# Problem 28: Number Spiral Diagonals
 
-side = 1001
-n = side**2
-diagonals = [n]
+def main(side):
 
-for x in range(int((side-1)/2)):
-    for y in range(4):
-        n = n - ((side-1)-2*x)
-        diagonals.append(n)
+	# Returns the sum of the numbers on the diagonal of a spiral square
+	# of side length side
+	if side % 2 == 0:
+		raise Exception("Side length must be odd!")
+	n = side**2
+	diagonals = [n]
+	for x in range(int((side-1)/2)):
+	    for y in range(4):
+	        n = n - ((side-1)-2*x)
+	        diagonals.append(n)
 
-print(sum(diagonals))
-    
+	return(sum(diagonals))
+
+print main(1001)
+	    
