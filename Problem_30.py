c17ea7a9927ad0316b10f9c290d1295ec4411f72
Grae Drake <graemdrake@gmail.com>
2014-01-12 23:01:20 -0500

    Refactor Problem 30

--- Problem_30.py
+++ Problem_30.py
@@ -1,12 +1,17 @@
-        # Problem 29: Distinct PowersDigit Fifth Powers
+# Problem 29: Distinct PowersDigit Fifth Powers
 
-results = []
+import time
+t0 = time.clock()
 
-for x in range(2, 1000000):
-    listx = [int(y) for y in str(x)]
-    powers = [y**5 for y in listx]
-    sumofpowers = sum(powers)
-    if sumofpowers == x:
-        results.append([x, sumofpowers])
+def main(power):
+	results = []
 
-print(results)
+	for x in range(2, 10 ** (power + 1)):
+	    listx = [int(y) for y in str(x)]
+	    sumofpowers = sum([y**5 for y in listx])
+	    if sumofpowers == x:
+	        results.append(x)
+	return sum(results)
+
+print main(5)
+print "Execution time: " + str(time.clock() - t0)[:5] + " seconds."
