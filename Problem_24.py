b1874585c7e21c9070468b93b84a8ae9ff1f17e9
Grae Drake <graemdrake@gmail.com>
2014-01-12 19:13:19 -0500

    Refactor Problem 24

--- Problem_24.py
+++ Problem_24.py
@@ -1,4 +1,10 @@
-        # Problem 24: Lexicographic permutations
+# Problem 24: Lexicographic permutations
+
 import itertools
-nums = [0,1,2,3,4,5,6,7,8,9]
-print(([x for x in itertools.permutations(nums)])[999999])
+
+def nth_lexigraphic_permutation(n, objects):
+
+	# Returns a tuple containing the nth lexigraphic permutation of objects
+	return [x for x in itertools.permutations(objects)][n-1]
+
+print nth_lexigraphic_permutation(1000000, [0,1,2,3,4,5,6,7,8,9])
