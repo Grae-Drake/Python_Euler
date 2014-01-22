48403d4e90f440ecc3a4924002357eb2c303b111
Grae Drake <graemdrake@gmail.com>
2014-01-21 23:16:38 -0500

    Problem 85 solved, needs refactor

--- Problem_85.py
+++ Problem_85.py
@@ -1,36 +1,39 @@
 # Problem 85: Counting rectangles
 
-def count_rectangles(x,y):
+import math
 
-	# Returns the number of component rectangles in a rectangular x by y grid.
-	return x * (x+1) * y * (y+1) / 4
+def count_rectangles(height,width):
+
+	# Returns the number of component rectangles in a rectangular height by width grid.
+	return height * (height+1) * width * (width+1) / 4
 
 def bracket_target(limit, height):
 
-	# Returns a tuple containing the largest count_rectangles(height, x) less
-	# than limit and the smallest count_rectangles(height, x) greater than limit.
+	# Returns a tuple containing the largest count_rectangles(height, width) less
+	# than limit and the smallest count_rectangles(height, width) greater than limit.
 
 	testnum = 0
-	x = 1
+	width = 0
 	while testnum < limit:
-		testnum = count_rectangles(height, x)
-		x += 1
-	return (count_rectangles(height, x-1), count_rectangles(height, x-2))
-
-
-
+		width += 1
+		testnum = count_rectangles(height, width)
+		
+	return ([count_rectangles(height, width), height * width], [count_rectangles(height, width-1), height * (width-1)])
 
 def main(limit):
+	rectangles = 0
+	area = None
 
-	# Finds the x by y grid with component rectangles closest to limit.
-	# Starts with a grid one square high.
+	# Here we calculate the root of the quadratic equation in count_rectangles()
+	# with height set to 1 and round up.
+	side = int(math.ceil(((8 * limit + 1) ** .5 -1) / 2))
 
-	results = []
-	for x in range(1414):
+	for x in range(1, side + 1):
 		for y in bracket_target(limit, x):
-			results.append(y)
-	return sorted(results)[-1]
+			if abs(limit - y[0]) < abs(limit - rectangles):
+				rectangles = y[0]
+				area = y[1]
 
+	return area
 
-print main(100000)
-
+print main(2000000)
