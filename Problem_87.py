507baface25cf6aab26b012bbe960b40dbed9818
Grae Drake <graemdrake@gmail.com>
2014-01-24 23:07:55 -0500

    Fiddling with stuff

---
+++ Problem_87.py
@@ -0,0 +1,15 @@
+# Problem 87: Prime Power Triples
+
+import Euler
+
+def main(limit)
+
+	fourth_power = [x ** 4 for x in Euler.prime_sieve(int(limit **.25))]
+	third_power = [x ** 4 for x in Euler.prime_sieve(int(limit ** (1/3)))]
+	second_power = [x ** 4 for x in Euler.prime_sieve(int(limit ** .5))]
+	print len(fourth_power)
+	print len(third_power)
+	print len(second_power)
+
+main(50000000)
+
