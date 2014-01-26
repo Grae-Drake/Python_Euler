4b6d2f501f7726e4a5cd8908d709f2b5ed9b4817
Grae-Drake <graemdrake@gmail.com>
2014-01-26 02:06:36 -0500

    Problem 88 getting wrong answer.  Why?

--- Problem_88.py
+++ Problem_88.py
@@ -4,4 +4,43 @@ import  Euler, time
 
 t0 = time.clock()
 
-print Euler.prime_factors(9)
+def main(limit):
+	result = 0
+	k = 0
+	n = 4
+	primes = [2]
+
+	def prime_factors(n):
+
+	    # Return a list of the prime factors of n.
+	    while n > primes[-1]:
+	    	primes.append(Euler.next_prime(primes[-1]))
+	    candidates = primes
+	    result = []
+	    for prime in candidates:
+	        while True:
+	            if n % prime == 0:
+	                result.append(prime)
+	                n = n / prime
+	            else:
+	                break
+	    return result
+
+	while k < limit:
+		n_factors = prime_factors(n)
+		if len(n_factors) == 0:
+			pass
+		else:
+			set_length = len(n_factors) + n - sum(n_factors)
+			if set_length > k:
+				k = set_length
+				result += n
+				print "For k = {}, n = {}.".format(k,n)
+		n += 1
+	return result
+
+print main(12)
+
+print "Execution time: " + str(time.clock() - t0)[:5] + " seconds."
+
+
