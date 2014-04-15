dfd09b634fa1eccac7712d57e88fdb4fa99e9a63
Grae Drake <graemdrake@gmail.com>
2014-04-15 00:58:53 -0400

    Fiddling with Problem 88

--- Problem_88.py
+++ Problem_88.py
@@ -1,13 +1,32 @@
 # Problem 88: Product-sum numbers
 
-import  Euler, time
+import  Euler, time, operator
 
 t0 = time.clock()
 
-def main(limit):
+def get_start_set(n):
+	"""For a given n, returns a list of numbers of length n. The first element
+	is equal to n, the second element is 2, and all remaining elements are 1's.
+	"""
+
+	return [n, 2] + [1 for x in range(n-2)]
+
+def sum_equals_product(set):
+	"""Returns True if the sum of the elements in set equals the product of the
+	elements in set."""
+
+	def prod(iterable):
+		return reduce(operator.mul, iterable, 1)
+
+	if sum(set) == prod(set):
+		return True
+	else:
+		return False
+
+for x in range(2,13):
+	print get_start_set(x)
 	
 
-print main(12)
 
 print "Execution time: " + str(time.clock() - t0)[:5] + " seconds."
 
