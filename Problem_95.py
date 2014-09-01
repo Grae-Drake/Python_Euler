6a71236118875f81223cdb33f1d313012492b8dc
Grae Drake <graemdrake@gmail.com>
2014-09-01 13:11:36 -0400

    Main function complete, debugging

--- Problem_95.py
+++ Problem_95.py
@@ -2,7 +2,7 @@
 
 import time
 
-limit = 1000000
+limit = 16000
 
 def sieve_proper_factors(limit):
 	# Returns a dictionary with keys from 1 to limit with corresponding sets
@@ -24,22 +24,20 @@ def make_lookup(limit):
 		lookup[number] = sum(factors)
 	return lookup
 
-def get_cycle(lookup, n):
+def get_cycle(n, lookup, seen):
+	# If a new cycle is detected it returns a list of cycle elements.
 	# Returns False if the proper factor sum chain is outside lookup or ends
-	# without repeating.  If a cycle is detected it returns a list of cycle
-	# elements.
-
-	# Could refactor to prevent duplicate positives
+	# without repeating.  Also returns False if the next sum in the chain has
+	# already been seen.
 
 	cycle_list = [n]
 	cycle_set = set([n])
 	next = lookup[n]
 	while True:
-		print next
-		if next == 1 or next not in lookup:
+		if next == 1 or next not in lookup or next in seen:
 			return False
 		if next in cycle_set:
-			return cycle_list[cycle_list.index(next):]
+			return set(cycle_list[cycle_list.index(next):])
 		else:
 			cycle_set.add(next)
 			cycle_list.append(next)
@@ -50,11 +48,23 @@ def get_cycle(lookup, n):
 
 
 def main(limit):
-	print get_cycle(make_lookup(16000), 12496)
+	seen = set([1])
+	result = {"length": None, "smallest": None}
+	for x in xrange(limit):
+		new_cycle = get_cycle(limit, make_lookup(limit), seen)
+		if new_cycle:
+			seen = seen.union()
+			if len(new_cycle) > result["length"]:
+				result["length"] = len(new_cycle)
+				result["smallest"] = min(new_cycle)
+	return result
+
+
 
 
 if __name__ == '__main__':
 	t1 = time.clock()
 	testing = main(limit)
+	print testing
 	print "Execution time: {} seconds".format(time.clock() - t1)
 
