62ce671f2e139181609d14d2edd235cbce66b968
Grae Drake <graemdrake@gmail.com>
2014-09-01 01:10:57 -0400

    Sieve seems to work

--- Problem_95.py
+++ Problem_95.py
@@ -1,6 +1,6 @@
 # Problem 95: Amicable chains
 
-import math, time
+import time
 
 limit = 1000000
 
@@ -8,33 +8,23 @@ chains = {}
 
 lookup = {}
 
-def calc_proper_factors(n):
-	# Returns a set of proper factors of n
-	answer = set([1])
-	for x in range(2, int(math.sqrt(n) + 1)):
-		if n % x == 0:
-			answer.add(x)
-			answer.add(x / n)
-	return answer
+def sieve_proper_factors(limit):
+	# Returns a dictionary with keys from 1 to limit with corresponding sets
+	# containing each key's proper factors
 
-def sum_proper_factors(n):
-	# Returns the sum of the proper factors of n
-	return sum(calc_proper_factors(n))
+	numbers = [set([1]) for x in xrange(limit)]
+	start_index = 2
+	for x in xrange(start_index, limit):
+		multiplier = 2
+		while multiplier * x < limit:
+			numbers[multiplier * x].add(x)
+			multiplier += 1
+	return numbers
 
-def find_chain(n):
-	# Returns the elements of the amicable chain starting with n
-	trail = set([n])
-	checking = sum_proper_factors(n)
-	while checking not in trail:
-		trail.add(checking)
-		checking = sum_proper_factors(checking)
-	return trail
 
 def main(limit):
-	for x in range(1, limit):
-		chains[x] = sum_proper_factors(x)
-	# for x,y in chains.items():
-	# 	print x, y
+	print len(sieve_proper_factors(limit))
+
 
 if __name__ == '__main__':
 	t1 = time.clock()
