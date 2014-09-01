1b544149f0de3b52ed15d32763572734087f1f49
Grae Drake <graemdrake@gmail.com>
2014-09-01 02:03:51 -0400

    Basic cycle detection

--- Problem_95.py
+++ Problem_95.py
@@ -24,8 +24,33 @@ def make_lookup(limit):
 		lookup[number] = sum(factors)
 	return lookup
 
+def get_cycle(lookup, n):
+	# Returns False if the proper factor sum chain is outside lookup or ends
+	# without repeating.  If a cycle is detected it returns a list of cycle
+	# elements.
+
+	# Could refactor to prevent duplicate positives
+
+	cycle_list = [n]
+	cycle_set = set([n])
+	next = lookup[n]
+	while True:
+		print next
+		if next == 1 or next not in lookup:
+			return False
+		if next in cycle_set:
+			return cycle_list[cycle_list.index(next):]
+		else:
+			cycle_set.add(next)
+			cycle_list.append(next)
+			next = lookup[next]
+
+
+
+
+
 def main(limit):
-	return make_lookup(limit)
+	print get_cycle(make_lookup(16000), 12496)
 
 
 if __name__ == '__main__':
