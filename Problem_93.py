a56ef7bbae5e68e8ba0497979b6a2348281a769c
Grae Drake <graemdrake@gmail.com>
2015-01-04 13:14:46 -0500

    Reduce time complexity of lookup table gen

--- Problem_93.py
+++ Problem_93.py
@@ -9,35 +9,45 @@ digits = range(1, 10)
 field = range(1, limit)
 
 # Build empty dictionaries for decomposing a number into component numbers
-# With field = 100 these take 320 milliseconds to populate.
+# With field = 100 these take 17 milliseconds to populate.
 
-subtraction = {digit: [] for digit in field}
-addition = {digit: [] for digit in field}
-division = {digit: [] for digit in field}
-multiplication = {digit: [] for digit in field}
+subtraction = {}
+addition = {}
+division = {}
+multiplication = {}
 
-for digit in field:
-    for x in field:
-        for y in field:
-            if y > x:
+for x in field:
+    for y in field:
+        if y > x:
 
-                if x - y == digit:
-                    subtraction[digit].append([x, y])
+            sub = y - x
+            if sub not in subtraction:
+                subtraction[sub] = []
+            subtraction[sub].append(sorted([x, y]))
 
-                if x + y == digit:
-                    addition[digit].append([x, y])
+            add = y + x
+            if add not in addition:
+                addition[add] = []
+            addition[add].append(sorted([x, y]))
 
-                if y / x == digit:
-                    division[digit].append([x, y])
+            mult = y * x
+            if mult not in multiplication:
+                multiplication[mult] = []
+            multiplication[mult].append(sorted([x, y]))
 
-                if x * y == digit:
-                    multiplication[digit].append([x, y])
+            if y % x == 0:
+                div = y / x
+                if div not in division:
+                    division[div] = []
+                division[div].append(sorted([x, y]))
 
+print division[20]
 print time.clock() - t1
 
 
 # Ok, we've built the decomposition dicts, time to start using them!
 # More to come here.
-for x in field:
-    if len(division[x][0]) == 1:
-        print x
+
+# for x in field:
+#     if len(division[x][0]) == 1:
+#         print x
