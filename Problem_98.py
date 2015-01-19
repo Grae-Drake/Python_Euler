28acaa1603500021a25cb39161ab1b04f9d58dd8
Grae Drake <graemdrake@gmail.com>
2015-01-19 12:46:58 -0500

    Builds word dictionary

--- Problem_98.py
+++ Problem_98.py
@@ -16,3 +16,43 @@
 
     NOTE: All anagrams formed must be contained in the given text file.
 """
+
+import time
+
+
+def main():
+
+    # Read the given wordfile and populate a list.
+    with open("TextFiles/p098_words.txt") as word_file:
+        words_list = word_file.readlines()[0].rsplit(',')
+
+    # Strip out words with more than 10 unique letters.
+    print len(words_list)
+    words_list = [x for x in words_list if len(set(x)) <= 10]
+    print len(words_list)
+
+    # Group words of similar length together in a dictionary.
+    longest_word = 0
+
+    for word in words_list:
+        longest_word = max(longest_word, len(word))
+
+    word_dict = {n: [] for n in range(1, longest_word + 1)}
+
+    for word in words_list:
+        word_dict[len(word)].append(word)
+
+    print word_dict[16]
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    t1 = time.clock()
+
+    print main()
+
+    print time.clock() - t1
